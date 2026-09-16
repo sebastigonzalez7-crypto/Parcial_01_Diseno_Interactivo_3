0e3a752036ab33257196b7205e801be49ae5dcbb
sebastian <sebastigonzalez7@gmail.com>
2026-09-15 20:57:24 -0500

    first commit

--- Parcial_Uno/parcial_uno.js
+++ Parcial_Uno/parcial_uno.js
@@ -1,22 +1,22 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null
+const nuevaString = "Hola Mundo"
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null
+const nuevoNum = 9
 
 // Crea una variable booleana:
-const nuevoBool = null
+const nuevoBool = true
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5
+const nuevaResta = 10 - 5 === 5
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40
+const nuevaMultiplicacion = 10 * 4 === 40
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % null === 1
+const nuevoModulo = 21 % 4 === 1
 
 // En los próximos 22 problemas, deberás completar la función.
 // Todo tu código irá dentro de las llaves de la función.
@@ -59,72 +59,110 @@ function sonIguales (x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
+  if( x === y ) {
+    return true
+  }else{
+    return false
+  }
 }
 
 function tienenMismaLongitud (str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(str1.length === str2.length){
+    return true
+  }else{
+    return false
+  }
 }
 
 function menosQueNoventa (num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (num < 90){
+    return true
+  }else{
+    return false
+  }
 }
 
 function mayorQueCincuenta (num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (num > 50){
+    return true
+  }else{
+    return false
+  }
+
 }
 
 function obtenerResto (x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
+  return x % y;
 }
 
 function esPar (num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(num % 2 === 0){
+    return true
+  }else{
+    return false
+  }
 }
 
 function esImpar (num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(num % 2 === 1){
+    return true
+  }else{
+    return false
+  }
 }
 
 function elevarAlCuadrado (num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
+  return num * num
 }
 
 function elevarAlCubo (num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
+  return num ** 3
 }
 
 function elevar (num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
+  return Math.pow(num, exponent)
 }
 
 function redondearNumero (num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
+  return Math.round(num)
 }
 
 function redondearHaciaArriba (num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
+  return Math.ceil(num)
 }
 
 function numeroRandom () {
   // Generar un número al azar entre 0 y 1 y devolverlo
   // Pista: investigá qué hace el método Math.random()
+  return Math.random()
 }
 
 function esPositivo (numero) {
